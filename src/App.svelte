11209485128baaf88eab14563a6a6d63848dff9e
fkrmubaroq <fkrmubaroq@gmail.com>
2024-11-28 16:57:20 +0700

    initial commit

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import TaskForm from "./lib/components/task-form.svelte";
+  import type { Task } from "./types";
+
+  let message = "Hello, World!";
+  let tasks = $state<Task[]>([]);
+
+  const addTask = (newTask: string) => {
+    console.log(newTask);
+    tasks.push({
+      id: crypto.randomUUID(),
+      title: newTask,
+      done: false,
+    });
+  };
+</script>
+
+<main>
+  <h1>{message}</h1>
+  <TaskForm {addTask} />
+  <section>
+    {#each tasks as task}
+      <article>{task.title}</article>
+    {/each}
+  </section>
+</main>
+
+<style>
+  main {
+    margin: 1rem;
+    max-width: 800px;
+  }
+</style>
---
+++ src/lib/components/task-form.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  let newTask = $state("");
+  let { addTask }: { addTask: (newTask: string) => void } = $props();
+
+  const onSubmit = (event: Event) => {
+    event.preventDefault();
+    console.log(newTask);
+    addTask(newTask);
+    newTask = "";
+  };
+</script>
+
+<form onsubmit={onSubmit}>
+  <label for="task">Task</label>
+  <input id="task" bind:value={newTask} />
+  <div class="button-container">
+    <button>Add Task</button>
+  </div>
+</form>
+
+<style>
+  input {
+    width: 100%;
+    border: 1px solid #ddd;
+    padding: 0.5rem;
+    border-radius: 0.25rem;
+  }
+  .button-container {
+    display: flex;
+    justify-content: end;
+    button {
+      @apply bg-blue-600 p-2;
+        
+    }
+  }
+</style>
